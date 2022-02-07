6deebd086d89f91bdee4775aabe370b2de8dfe33
Felipe Rodas <ae86rodas@outlook.com>
2022-02-07 07:48:43 -0500

    test 4

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const piblaster = require("pi-blaster.js")
 
 piblaster.setPwm(17, 0.2)
 
-// setTimeout(()=>{
-//     piblaster.setPwm(4, 0)
-// }, 2000)
+setTimeout(()=>{
+    piblaster.setPwm(4, 0.5)
+}, 2000)
+
+setTimeout(()=>{
+    piblaster.setPwm(4, 0)
+}, 4000)
