2ea3b447ec214fb62d22fa7d444f205cfbb2bde4
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-02-07 18:07:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors")
 const app = express();
+const piblaster = require("pi-blaster.js")
 const PORT = process.env.PORT || 3000;
 
 app.use(cors())
@@ -8,14 +9,17 @@ app.use(cors())
 app.get("/up",(req, res) => {
     console.log("up command recived")
     res.send("robot moving up")
+    piblaster.setPwm(17, 0.06)
 })
 app.get("/down",(req, res) => {
     console.log("down command recived")
     res.send("robot moving down")
+    piblaster.setPwm(17, 0.24)
 })
 app.get("/right",(req, res) => {
     console.log("right command recived")
     res.send("robot moving right")
+    piblaster.setPwm(17, 0.15)
 })
 app.get("/left",(req, res) => {
     console.log("left command recived")
