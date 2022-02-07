9624dd4a2725341f89a99e28c388f3771a43a919
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-02-07 16:20:56 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const piblaster = require("pi-blaster.js")
 
-piblaster.setPwm(17, 0.2)
+piblaster.setPwm(17, 0.8)
 
-setTimeout(()=>{
-    piblaster.setPwm(17, 0.5)
-}, 2000)
+// setTimeout(()=>{
+//     piblaster.setPwm(17, 0.5)
+// }, 2000)
 
-setTimeout(()=>{
-    piblaster.setPwm(17, 0)
-}, 4000)
+// setTimeout(()=>{
+//     piblaster.setPwm(17, 0)
+// }, 4000)
