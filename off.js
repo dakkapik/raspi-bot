39ea09ff8b6b06df2db2b6a0a2b62e8819bbbd0e
Felipe Rodas <ae86rodas@outlook.com>
2022-02-07 07:43:36 -0500

    test 3

--- off.js
+++ off.js
@@ -1,6 +1,6 @@
 const piblaster = require("pi-blaster.js")
 
-piblaster.setPwm(4, 0)
+piblaster.setPwm(17, 0)
 
 // setTimeout(()=>{
 //     piblaster.setPwm(4, 0)
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const piblaster = require("pi-blaster.js")
 
-piblaster.setPwm(4, 0.2)
+piblaster.setPwm(17, 0.2)
 
 // setTimeout(()=>{
 //     piblaster.setPwm(4, 0)
