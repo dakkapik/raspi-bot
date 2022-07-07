a2006ee1f7ce6e4263d7b5972f03e587c8d47739
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-07-07 19:31:46 -0400

    misc

--- Classes/Gyroscope.js
+++ Classes/Gyroscope.js
@@ -8,41 +8,61 @@ module.exports = class Gyroscope {
             y: 0,
             z: 0,
         }
-        this.tressholdRepo = []
-        this.tresshold = undefined
-        this.tressholdSmplSize = 200
-        this.tressholdMult = 2
+        this.sampleRepo = {
+            x: [],
+            y: [],
+            z: []
+        }
+        this.mean = {};
+        this.standardDeviation = {};
+        this.filter = undefined;
+        this.iteration = 0;
+        this.popSize = 200;
+        this.diviationMultiplier = 2;
+
     }
 
     updateG (x, y, z) {
-        if(this.tresshold === undefined){
-            this.setTresshold({x,y,z})
+        if(this.filter === undefined){
+            this.updateRepo(x,y,z)
         } else {
-            if(Math.abs(x) > this.tresshold.x) this.angles.x += x
-            if(Math.abs(y) > this.tresshold.y) this.angles.y += y * 5
-            if(Math.abs(z) > this.tresshold.z) this.angles.z += z
+            if(x > this.filter.x.top || x < this.filter.x.bottom) this.angles.x += x
+            if(y > this.filter.y.top || y < this.filter.y.bottom) this.angles.y += y
+            if(z > this.filter.z.top || z < this.filter.z.bottom) this.angles.z += z
         }
     }
 
-    setTresshold (angles) {
-        if(this.tressholdRepo.length < this.tressholdSmplSize) {
-            this.tressholdRepo.push(angles)
-        } else if ( this.tressholdRepo.length === this.tressholdSmplSize ){
-            const tresshold = {x: 0, y: 0, z: 0}
-
-            this.tressholdRepo.forEach((a, i) => {
-                tresshold.x += Math.abs(a.x)
-                tresshold.y += Math.abs(a.y)
-                tresshold.z += Math.abs(a.z)
-                if(i === this.tressholdRepo.length - 1){
-                    this.tresshold = {}
-                    this.tressholdRepo = []
-                    this.tresshold.x = (tresshold.x / this.tressholdSmplSize) * this.tressholdMult
-                    this.tresshold.y = (tresshold.y / this.tressholdSmplSize) * this.tressholdMult
-                    this.tresshold.z = (tresshold.z / this.tressholdSmplSize) * this.tressholdMult
-                    console.log("tresshold set")
-                }
-            })
+    updateRepo(x,y,z) {
+        if(this.iteration < this.popSize){
+            this.sampleRepo.x.push(x)
+            this.sampleRepo.y.push(y)
+            this.sampleRepo.z.push(z)
+            this.iteration ++;
+        } else {
+            this.filter = {}
+            this.calcAxisFilter("x")
+            this.calcAxisFilter("y")
+            this.calcAxisFilter("z")
         }
+    }
+
+    calcAxisFilter( axis ) {
+        const sampleData = this.sampleRepo[axis]
+
+        let sumTotal = 0;
+        // check if pop size and iteration are the same
+        console.log("xdata length: " , sampleData.length)
+        console.log("iteration: " , this.iteration)
+
+        for(let i = 0; i < sampleData.length; i++) sumTotal += sampleData[i]
+
+        this.mean[axis] = sumTotal / sampleData.length
+
+        let stdDiv = 0
+        sampleData.forEach(value => stdDiv += (value - this.mean[axis])*(value - this.mean[axis]))
+        stdDiv = Math.sqrt(stdDiv / (sampleData.length - 1))
+
+        this.standardDeviation[axis] = stdDiv;
+        this.filter[axis] = {top: this.mean[axis] + stdDiv * this.diviationMultiplier, bottom: this.mean[axis] - stdDiv * this.diviationMultiplier}
     }
 }
