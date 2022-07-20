ec3f7b6918220254ffe6a1b56976fc464ecfb89c
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-07-19 20:45:16 -0400

    bootrap

---
+++ turret/index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const { io } = require("socket.io-client")
+const { stdout, stderr } = require('node:process');
+const { spawn } = require("child_process");
+
+const child = spawn('./a', [] , {
+    stdio: ['pipe','pipe', stderr],
+    cwd: path.resolve(path.join(__dirname,'src'))
+})
+
+child.stdout.pipe(stdout)
+
+const socket = io ("http://192.168.0.210:5000")
+/// your ip ^^^^^^^
+
+socket.on("connect",() => {
+    socket.emit("purpose", "turret")
+})
+
+socket.on("canvas-pos", ( pos )=> {
+    Buffer.from(pos).pipe(child.stdin)
+})
